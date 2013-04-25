26783c07d98587f30b0b417a98ed8bd98a013c86
Dheeraj Oswal <dheeraj-oswal@healthlucid.com>
2013-04-25 19:15:47 +0530

    create Apriori rule list

---
+++ resources/run_Apriori.sh
@@ -0,0 +1,1 @@
+mvn exec:java -Dexec.mainClass=AssociationRules -Dexec.args=data_sets/BGL/blue_gene.arff 
---
+++ src/main/java/predictor.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: ameyamore
+ * Date: 25/04/13
+ * Time: 7:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class predictor {
+}
