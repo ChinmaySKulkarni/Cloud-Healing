da1f69dfc241c1ae0cbb692263d20b6ce81d871b
Ameya More <dheeraj-oswal@healthlucid.com>
2013-05-01 15:45:35 +0530

    Add serializable apriori rules and run apriori script

--- resources/run_Apriori.sh
+++ resources/run_Apriori.sh
@@ -1,1 +1,4 @@
-mvn exec:java -Dexec.mainClass=AssociationRules -Dexec.args=data_sets/BGL/blue_gene.arff 
+export MAVEN_OPTS="-Xmx3000m -Xms2048m -XX:MaxPermSize=512"
+mvn exec:java -Dexec.mainClass=AssociationRulesGenerator -Dexec.args=data_sets/BGL/blue_gene.arff
+mvn exec:java -Dexec.mainClass=Predictor
+mvn exec:java -Dexec.mainClass=Grapher
